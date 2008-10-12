b637167ced19e31189b17c16bc6696c44b431074
Sam Rowe <sam@usg.edu>
2008-10-12 19:22:48 -0400

    added copyright to debug.php
    I'd forgotten it was seperate previously

--- jart/debug.php
+++ jart/debug.php
@@ -1,3 +1,8 @@
+// Copyright (C) 2008 Board of Regents of the University System of Georgia
+//
+// This file is part of YaketyStats (see http://yaketystats.org/).
+// YaketyStats is free software: you can redistribute it and/or modify
+
 var debugcount = 0;
 var user = 'sam';
 var debugLogfiles = <?php $o = debugLogfiles(); print($o); ?>
