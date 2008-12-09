d20ce86428eacf599ff9de6508cec9f247f5d40c
Sam Rowe <sam@benny.bor.usg.edu>
2008-12-09 15:05:50 -0500

    versions!

--- jart/index.php
+++ jart/index.php
@@ -1217,7 +1217,7 @@ if ( in_array($_SERVER['PHP_AUTH_USER'],$admins) ){
 call_user_func_array('sajax_export',$exports);
 //sajax_export(&$exports);
 sajax_handle_client_request();
-$version = "2.1";
+$version = "2.2pre";
 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
