124aaf6783656b1e6cfa261dcda23fc21d2394ee
Sam Rowe <sam@usg.edu>
2009-01-23 09:13:36 -0500

    fix 150

--- jart/index.php
+++ jart/index.php
@@ -1624,6 +1624,7 @@ $version = "2.2pre";
                     G.addRrdToGraph(path,0);
                 });
             });
+            G.graphs[0].regex = $F('regextext');
             Element.show('regexersaver');
             //console.log(G.graphs);
         }
