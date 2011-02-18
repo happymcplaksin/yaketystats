053625b61b9796b7ed7a2b7ae0498061192b6e57
Sam Rowe <github@samrowe.com>
2011-02-18 16:35:10 -0500

    sheesh

--- collector/plugins/collectorstats.rb
+++ collector/plugins/collectorstats.rb
@@ -18,8 +18,8 @@ class Collectorstats
             # Oh hai. Everyone knows the page size is 4096, dummo.
             out << gauge(:p => "collector/memory/rss", :v => @data.rss.*(4096))
             out << gauge(:p => "collector/memory/vsz", :v => @data.vsize)
-            out << counter(:p => "collector/cpu/user", :v => @data.utime.(100))
-            out << counter(:p => "collector/cpu/sys",  :v => @data.stime.(100))
+            out << counter(:p => "collector/cpu/user", :v => @data.utime.*(100))
+            out << counter(:p => "collector/cpu/sys",  :v => @data.stime.*(100))
         else
             raise YS::NoData
         end
